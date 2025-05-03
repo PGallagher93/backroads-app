2f2976929eac414ed1c42de17cc58c11aaad0049
PGallagher93 <Pgallagher93@hotmail.com>
2025-05-03 11:16:48 +0100

    corrects img imports

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import About from "./components/About"
 import Services from "./components/Services"
 import Tours from "./components/Tours"
 import Footer from "./components/Footer"
-
+import Hero from "./components/Hero"
 function App() {
   return (
     <>
     <Navbar/>
+    <Hero/>
     <About/>
     <Services />
     <Tours/>

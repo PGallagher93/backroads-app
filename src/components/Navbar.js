f9b89f5ed667a3b4820bdf946f7f4bda730a9996
PGallagher93 <Pgallagher93@hotmail.com>
2025-05-03 11:12:43 +0100

    corrects social links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => {
           
           return (
             <li key={link.id}>
-          <a href={link.href} target="_blank" className="nav-icon"
+          <a href={link.href} target="_blank" rel="noreferrer" className="nav-icon"
             ><i className={link.icon}></i></a>
         </li>
           )
